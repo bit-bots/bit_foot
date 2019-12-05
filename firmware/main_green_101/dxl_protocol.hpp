218c78dea08624cd9ae4cec8351a73d6b19d8ba7
Jan-Tarek Butt <tarek@ring0.de>
2019-12-05 23:53:27 +0100

    dxl_protocol.hpp: rm trail spaces
    - use 'uint16_t' instead of 'unsigned short'
    - use constexpr instead of #define
    - do not declare C-style arrays, use std::array<> instead

--- firmware/main_green_101/dxl_protocol.hpp
+++ firmware/main_green_101/dxl_protocol.hpp
@@ -5,23 +5,24 @@
 #define DXL_PROTOCOL_HPP
 
 #include "dxl.hpp"
+#include <array>
 
-#define DXL_PING        0x01
-#define DXL_READ_DATA   0x02
-#define DXL_WRITE_DATA  0x03
-#define DXL_REG_WRITE   0x04
-#define DXL_ACTION      0x05
-#define DXL_RESET       0x06
-#define DXL_REBOOT      0x08
+constexpr unsigned int DXL_PING       = 0x01;
+constexpr unsigned int DXL_READ_DATA  = 0x02;
+constexpr unsigned int DXL_WRITE_DATA = 0x03;
+constexpr unsigned int DXL_REG_WRITE  = 0x04;
+constexpr unsigned int DXL_ACTION     = 0x05;
+constexpr unsigned int DXL_RESET      = 0x06;
+constexpr unsigned int DXL_REBOOT     = 0x08;
 
-#define DXL_STATUS      0x55
+constexpr unsigned int DXL_STATUS     = 0x55;
 
-#define DXL_SYNC_READ   0x82
-#define DXL_SYNC_WRITE  0x83
-#define DXL_BULK_READ   0x92
-#define DXL_BULK_WRITE  0x93
+constexpr unsigned int DXL_SYNC_READ  = 0x82;
+constexpr unsigned int DXL_SYNC_WRITE = 0x83;
+constexpr unsigned int DXL_BULK_READ  = 0x92;
+constexpr unsigned int DXL_BULK_WRITE = 0x93;
 
-#define DXL_NO_ERROR    0x00
+constexpr unsigned int DXL_NO_ERROR   = 0x00;
 
 void dxl_process(
     volatile struct dxl_device *device,
@@ -34,24 +35,24 @@ void dxl_process(
 struct dxl_registers
 {
     volatile struct dxl_eeprom {
-        unsigned short  modelNumber;
+        uint16_t  modelNumber;
         unsigned char firmwareVersion;
         unsigned char id;
         unsigned char baudrate;
         unsigned char returnDelay;
-        unsigned short cwLimit;
-        unsigned short ccwLimit;
+        uint16_t cwLimit;
+        uint16_t ccwLimit;
         unsigned char _dummy;
         unsigned char temperatureLimit;
         unsigned char lowestVoltage;
         unsigned char highestVoltage;
-        unsigned short maxTorque;
+        uint16_t maxTorque;
         unsigned char returnStatus;
         unsigned char alarmLed;
         unsigned char alarmShutdown;
     } eeprom __attribute((packed));
 
-    volatile unsigned char _dummy2[4]; 
+    volatile std::array<unsigned char, 4> _dummy2;
 
     volatile struct dxl_ram {
         unsigned char torqueEnable;
@@ -60,19 +61,19 @@ struct dxl_registers
         unsigned char ccwComplianceMargin;
         unsigned char cwComplianceSlope;
         unsigned char ccwComplianceSlope;
-        unsigned short goalPosition;
-        unsigned short movingSpeed;
-        unsigned short torqueLimit;
-        unsigned short presentPosition;
-        unsigned short presentSpeed;
-        unsigned short presentLoad;
+        uint16_t goalPosition;
+        uint16_t movingSpeed;
+        uint16_t torqueLimit;
+        uint16_t presentPosition;
+        uint16_t presentSpeed;
+        uint16_t presentLoad;
         unsigned char presentVoltage;
         unsigned char presentTemperature;
         unsigned char registeredInstruction;
         unsigned char _dummy3;
         unsigned char moving;
         unsigned char lock;
-        unsigned short punch;
+        uint16_t punch;
     } ram __attribute__((packed));
 
     volatile char eeprom_dirty;
